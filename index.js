4df3c9ff2d56d4da568085959de71f1dff9a8342
Ross Hendry <rhendry@googlemail.com>
2016-06-25 21:39:25 +0100

    Linting and command checking
    Now checks for the existence of pdftk, fixed linting issues.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const promisify = require('es6-promisify');
 const fillPdf = require('fill-pdf');
 const _ = require('lodash');
+
+const commandExists = promisify(require('./lib/command-exists'));
 const CharacterService = require('./lib/character-service');
 const Sheets = require('./lib/sheets');
 
@@ -11,15 +13,28 @@ const campaignid = 'hoardofthedragon';
 const characterNames = ['cameron-du-lac'];
 const sheet = Sheets.get('basic-sheet');
 
-console.log(Sheets.list());
+const generateSheets = () => {
+  _.forEach(characterNames, (characterName) => {
+    const generateSheet = _.partialRight(generatePdf, sheet.filename);
+    const writeSheet = _.partial(fs.writeFile, `${characterName}.pdf`);
 
-_.forEach(characterNames, (characterName) => {
-  const generateSheet = _.partialRight(generatePdf, sheet.filename);
-  const writeSheet = _.partial(fs.writeFile, characterName + '.pdf');
+    CharacterService.get(campaignid, characterName)
+      .then(sheet.extractor)
+      .then(generateSheet)
+      .then(writeSheet)
+      .catch((err) => {
+        //eslint-disable-next-line
+        console.error(err);
+        throw err;
+      });
+  });
+};
 
-  CharacterService.get(campaignid, characterName)
-    .then(sheet.extractor)
-    .then(generateSheet)
-    .then(writeSheet);
-});
+commandExists('pdftk')
+  .then(generateSheets)
+  .catch(() => {
+    //eslint-disable-next-line
+    console.error('pdftk not found in path');
+    process.exit(1);
+  });
 
